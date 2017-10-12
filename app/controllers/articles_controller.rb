5c58028b899672ff3b86c68fbf9a31fc244be737
Stan <stanleytu1995@hotmail.com>
2017-10-12 14:57:22 -0500

    Resolved conflicts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,9 @@
 class ArticlesController < ApplicationController
-<<<<<<< HEAD
 
   def index
     @articles = Article.all
   end
 
-=======
->>>>>>> c93a7315a24ec8c6e5e3f2db7de5138407a66dc1
   def show
     @article = Article.find(params[:id])
   end
@@ -17,33 +14,21 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params[article_params])
-<<<<<<< HEAD
 
-    if @article.save
-      render plain: "Good"
-    else
-      render plain: "Error"
-    end
-    redirect_to @article
-=======
     if @article.save
        #do something if it saves
        redirect_to @article
     else
        render plain: "Something is wrong here."
-    end 
+    end
 
     #@article.save
     #redirect_to @article
->>>>>>> c93a7315a24ec8c6e5e3f2db7de5138407a66dc1
   end
 
   private
     def article_params
-<<<<<<< HEAD
       params.require(:article).permit(:title, :text)
-=======
-      params.permit(:title, :text)  
->>>>>>> c93a7315a24ec8c6e5e3f2db7de5138407a66dc1
+      params.permit(:title, :text)
     end
 end
